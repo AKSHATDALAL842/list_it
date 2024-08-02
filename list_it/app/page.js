7ea17a9c0e6702bd82fac49d5e397ebb23ea88a0
Akshat Dalal <63205271+AKSHATDALAL842@users.noreply.github.com>
2024-08-02 18:40:38 +0530

    0.1.1.5
    basic code before an error occur

--- list_it/app/page.js
+++ list_it/app/page.js
@@ -1,60 +1,87 @@
 'use client';
-import * as React from 'react';
-import Image from 'next/image';
 import { useState, useEffect } from 'react';
-import { Firestore } from '@/firebase';
-// import { initializeApp } from 'firebase/app';
-import { Box, Typography } from '@mui/material';
+import { firestore } from '@/firebase';
+import { Box, Stack, Typography} from '@mui/material';
+import {collection, deleteDoc, doc, getDocs, query} from 'firebase/firestore'
 
-export default function BoxBasic() {
+export default function Home() {
   const [inventory, setInventory] = useState ([])
   const[open, setOpen] = useState(false)
   const[itemName, setItemName] = useState('')
 
-  const UpdateInventory = async () =>{
-    const snapshot = query (collection (firestore, 'inventory'))
-    const docs = await getDocs (snapshot)
-    const inventoryList = []
-    docs. forEach ((doc) =>{
-      inventoryList.push({
-        name: doc.id, 
-        ...doc.data(),
-      })
+const updateInventory = async () =>{
+  const snapshot = query (collection (firestore, 'inventory'))
+  const docs = await getDocs (snapshot)
+  const inventoryList = []
+  docs. forEach ((doc) =>{
+    inventoryList.push({
+      name: doc.id, 
+      ...doc.data(),
     })
-    setInventory(inventoryList)
+  })
+  setInventory(inventoryList)
+}
+
+const addItem = async (item) => {
+  const docRef = doc (collection (firestore,'inventory'), item)
+  const docSnap = await getDoc (docRef)
+  if (docSnap.exists()){
+  const {quantity} = docSnap.data ()
+  await setDoc(docRef, {quantity: quantity + 1})
+  }else {
+    await setDoc(docRef, {quantity: 1})
   }
+  await updateInventory ()
+}
 
-  const removeItem = async (itemName) => {
-    const docRef = doc(collection(firestore, 'inventory'), item);
-    const docsnap = await getDoc(docRef);
-    if (docsnap.exists()) {
-      const {quantity} = docSnap. data ()
-      if(quantity === 1){
-        await deleteDoc(docRef)
-      }else{
-        await setDoc(docRef, {quantity: quantity - 1})
-      }
+const removeItem = async (item) => {
+  const docRef = doc(collection(firestore, 'inventory'), item);
+  const docsnap = await getDoc(docRef);
+  if (docsnap.exists()) {
+    const {quantity} = docSnap. data ()
+    if(quantity === 1){
+      await deleteDoc(docRef)
+    }else{
+      await setDoc(docRef, {quantity: quantity - 1})
     }
-    await updateInventory()
-  };
+  }
+  await updateInventory()
+}
 
-  useEffect (() => {
-    updateInventory()
-  }, [])
+useEffect (() => {
+  updateInventory()
+}, [])
 
-  return (
-    
-    <Box>
-      <Typography variant='h1'>Inventory Management</Typography>
-      {inventory. forEach ( (item) =>{
-        console.log(item)
-        return (
-          <Box>
-            {item.name}
-            {item.count}
-          </Box>
-        )
-      })}
-    </Box>
-  );
+const handleOpen = () => setOpen(true)
+const handleClose = () => setOpen(false)
+return (
+  <Box width="100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" gap={2}>
+    <Modal open={open} onClose={handleClose}>
+      <Box 
+      position="absolute" 
+      top="50%" 
+      left="50%"
+      width={400} 
+      bgcolor="white" 
+      border="2px solid #000" 
+      boxShadow={24} 
+      p={4} 
+      display="flex" 
+      displaydirection="column" 
+      gap={3}
+      sx={{
+        transform:'translate(-50%, -50%)' 
+      }}
+      >
+        <Typography variant="h6">Add Item</Typography>
+        <Stack width="100%" direction="row" spacing={2}>
+          <TextField value={ItemName} variant='outlined' fullWidth onChange={(e) => setItemName(e.target.value)}/>
+          <Button variant="coutlined" onClick={() => addItem(itemName)}>Add</Button>
+        </Stack>
+      </Box>
+    </Modal>
+    <Typography variant='h1'>Inventory Management</Typography>
+
+  </Box>
+)
 }
