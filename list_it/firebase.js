ebf0b97f73ef43242e5d65fd7d14f7ba53c56e92
Akshat Dalal <63205271+AKSHATDALAL842@users.noreply.github.com>
2024-08-04 20:53:48 +0530

    0.1.1.8
    updated the firebase.js with the token key but still cant add the items in inventory

--- list_it/firebase.js
+++ list_it/firebase.js
@@ -1,13 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import{getFirestore} from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- apiKey: "YOUR_API_KEY",
- authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
- projectId: "YOUR_PROJECT_ID",
- storageBucket: "YOUR_PROJECT_ID.appspot.com",
- messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
- appId: "YOUR_APP_ID"
- };
+  apiKey: "AIzaSyBqonQ-u5_uwt3fwhtZxZH1NDvXZJfX23Q",
+  authDomain: "pantry-app-969e3.firebaseapp.com",
+  projectId: "pantry-app-969e3",
+  storageBucket: "pantry-app-969e3.appspot.com",
+  messagingSenderId: "282738394306",
+  appId: "1:282738394306:web:6d35c41d9e0ef73e8ae75b",
+  measurementId: "G-XN12QJ3SMH"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-export { firestore };
+const Firestore = getFirestore(app);
+
+export {Firestore}
